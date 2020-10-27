ab99e1377328565b1bea89303f8477da25a80f8e
JayJin <unisquarebsm@gmail.com>
2020-10-27 19:37:15 +0900

    initial xcode project creation

---
+++ youtube_demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube_demo
+//
+//  Created by JUNGHUN JIN on 2020/10/27.
+//  Copyright © 2020 JayJin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
